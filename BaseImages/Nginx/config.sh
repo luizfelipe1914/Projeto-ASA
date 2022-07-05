bcb36e44fcfa0a1fc794b5e4308b7502f57b4a57
Luiz Felipe <luizfelipe1914@gmail.com>
2022-07-05 01:43:48 -0300

    .

--- BaseImages/Nginx/config.sh
+++ BaseImages/Nginx/config.sh
@@ -1,10 +1,7 @@
-cat /ssh > /etc/ssh/sshd_config 
-mkdir -p /root/.ssh
-mv authorized_keys /root/.ssh/
 touch /var/log/auth.log
 touch /var/log/syslog
 mkdir -p /var/log/nginx
-chown www-data:syslog /var/log/named/
+chown www-data:syslog /var/log/nginx/
 chown syslog:syslog /var/log/auth.log
 chown syslog:syslog /var/log/syslog
-rm -f config.sh ssh 
+rm -f config.sh 
